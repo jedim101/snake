7a319fc18de14322321a75f61b4ad0c78b8a90e8
matthewglasser <matthewsglasser@gmail.com>
2024-07-09 17:41:27 -0400

    realized I have no clue how godot works, so imma figure that out

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,14 +1,21 @@
 extends Node
 
+@export var snake_scene : PackedScene
+
 var score: int
 
-var cells: int = 20
-var cells_size: int = 50
+var snake_pos: Array
+
+var start_pos = Vector2(0, 0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	generate_snake()
 	pass # Replace with function body.
 
+func generate_snake():
+	snake_pos[0] += 1
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
