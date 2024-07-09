284d7ee196073ac9e7f508f55e73180f3a21a74b
matthewglasser <matthewsglasser@gmail.com>
2024-07-09 18:30:38 -0400

    keep moving in same direction

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 @export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
+var direction = Vector2(0, -1)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,18 +11,17 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
+		direction = Vector2(1, 0)
+	elif Input.is_action_pressed("move_left"):
+		direction = Vector2(-1, 0)
+	elif Input.is_action_pressed("move_down"):
+		direction = Vector2(0, 1)
+	elif Input.is_action_pressed("move_up"):
+		direction = Vector2(0, -1)
 
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
+	#if velocity.length() > 0:
+		#velocity = velocity.normalized() * speed
 	
-	position += velocity * delta
+	position += direction
 	position = position.clamp(Vector2.ZERO, screen_size)
