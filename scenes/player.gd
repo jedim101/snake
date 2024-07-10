746c04a4fc7904a9faa56c6d959934b726b3ab67
matthewglasser <matthewsglasser@gmail.com>
2024-07-10 17:54:05 -0400

    no continuous movement

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,13 +1,12 @@
 extends Area2D
 
-@export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
 var direction = Vector2(0, -1)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Timer.start()
 	screen_size = get_viewport_rect().size
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -20,8 +19,8 @@ func _process(delta):
 	elif Input.is_action_pressed("move_up"):
 		direction = Vector2(0, -1)
 
-	#if velocity.length() > 0:
-		#velocity = velocity.normalized() * speed
-	
-	position += direction
+func _on_Timer_timeout():
+	position += direction * 50
 	position = position.clamp(Vector2.ZERO, screen_size)
+
+	$Timer.start()
