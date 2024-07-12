69b8e86cf5a6bb9885e4fcbc41ed226cf12748b3
matthewglasser <matthewsglasser@gmail.com>
2024-07-11 22:06:04 -0400

    redid snake movement for easier collision detection

--- scenes/main.gd
+++ scenes/main.gd
@@ -3,18 +3,20 @@ extends Node
 @export var length: int
 var direction = Vector2(1, 0)
 var new_direction = Vector2(1, 0)
-# var board = []
+
+var start_position = Vector2(400, 400)
+
+var snake = [$Head]
 
 func _ready():
-	# for r in range(15):
-	# 	for c in range(20):
-	# 		board[r][c] = 0
-	
-	# board[8][10] = length
+	$Head.position = start_position
+
+	for i in range(5):
+		new_segment(start_position + Vector2(-(5 - i) * 40, 0))
 
 	$Timer.start()
 
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_pressed("move_right"):
 		new_direction = Vector2(1, 0)
 	elif Input.is_action_pressed("move_left"):
@@ -24,28 +26,28 @@ func _process(delta):
 	elif Input.is_action_pressed("move_up"):
 		new_direction = Vector2(0, -1)
 
-	$Head.position += direction * 40 * (delta / $Timer.wait_time)
+	# $Head.position += direction * 40 * (delta / $Timer.wait_time)
 
 
 func _on_Timer_timeout():
 	direction = new_direction
 
+	new_segment($Head.position)
+	snake.pop_at(1).queue_free()
+	
+	$Head.position += direction * 40
+
+	$Timer.start()
+
+func new_segment(pos):
 	var body_segment = ColorRect.new();
 	add_child(body_segment)
 
-	body_segment.position = $Head.position
+	body_segment.position = pos
 	body_segment.name = str(length - 1)
 
 	body_segment.size.x = 40
 	body_segment.size.y = 40
 	body_segment.color = "#13ac56"
 
-	for i in range(length):
-		var segment = get_node(str(i))
-		if segment:
-			segment.name = str(i - 1)
-	
-	var end_segment = get_node("-1")
-	if end_segment:
-		end_segment.queue_free()
-	$Timer.start()
+	snake.append(body_segment)
